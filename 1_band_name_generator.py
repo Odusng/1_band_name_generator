0219bdc795402cf6a2847d6de8a44b3f902233b8
Odunuga Oluwatosin <tosinodunuga009@gmail.com>
2025-04-05 23:37:45 +0100

    Add files via upload

---
+++ 1_band_name_generator.py
@@ -0,0 +1,5 @@
+greetings = ("Welcome to the Band Name Generator.")
+print(greetings)
+name_of_city = input("What's the name of the city you grew up in?\n")
+name_of_pet = input("What's your pet's name?\n")
+print("Your band name could be: " + name_of_city + " " + name_of_pet)
